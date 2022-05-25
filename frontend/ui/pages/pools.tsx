14a461129fe20f184b8b70964c85a01e5aa537fa
sencho <duelaliens@gmail.com>
2022-05-25 22:28:57 +0900

    replace logo

--- frontend/ui/pages/pools.tsx
+++ frontend/ui/pages/pools.tsx
@@ -92,11 +92,11 @@ const Home: NextPage = () => {
     <Layout>
 
       <Head>
-        <title>Solar Flares - Get an NFT on IC</title>
+        <title>SOLAR FLARES - Get an NFT on IC</title>
         <meta name="description" content="When the sun fills up with helium, a solar flare shoots in a random direction, at one contributor" />
         <link rel="icon" href="/favicon.ico" />
 
-        <meta property="og:title" content={"Solar Flares - Get an NFT on IC"} />
+        <meta property="og:title" content={"SOLAR FLARES - Get an NFT on IC"} />
         <meta property="og:type" content="website" />
         <meta property="og:description" content={'When the sun fills up with helium, a solar flare shoots in a random direction, at one contributor'} />
         <meta property="og:url" content="https://jdq7a-qiaaa-aaaad-qcbia-cai.ic.fleek.co/ogp.jpg" />
@@ -104,7 +104,7 @@ const Home: NextPage = () => {
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
 
-        <meta name="twitter:title" content={'Solar Flares - Get an NFT on IC'} />
+        <meta name="twitter:title" content={'SOLAR FLARES - Get an NFT on IC'} />
         <meta name="twitter:description" content={'When the sun fills up with helium, a solar flare shoots in a random direction, at one contributor'} />
         <meta name="twitter:image" content="https://jdq7a-qiaaa-aaaad-qcbia-cai.ic.fleek.co/ogp.jpg" />
         <meta name="twitter:card" content="summary_large_image" />
