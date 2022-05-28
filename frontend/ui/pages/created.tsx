c608f171e540192d449549ed9ef5f7c709d369fb
sencho <duelaliens@gmail.com>
2022-05-28 14:19:27 +0900

    OGP

--- frontend/ui/pages/created.tsx
+++ frontend/ui/pages/created.tsx
@@ -17,14 +17,14 @@ const Home: NextPage = () => {
         <meta property="og:title" content={"Your pools - SOLAR FLARES"} />
         <meta property="og:type" content="website" />
         <meta property="og:description" content={'When the sun fills up with helium, a solar flare shoots in a random direction, at one contributor'} />
-        <meta property="og:url" content="https://jdq7a-qiaaa-aaaad-qcbia-cai.ic.fleek.co/ogp.jpg" />
-        <meta property="og:image" content="https://jdq7a-qiaaa-aaaad-qcbia-cai.ic.fleek.co/ogp.jpg" />
+        <meta property="og:url" content="https://sf.duelaliens.com/ogp.jpg" />
+        <meta property="og:image" content="https://sf.duelaliens.com/ogp.jpg" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
 
         <meta name="twitter:title" content={'Your pools - SOLAR FLARES'} />
         <meta name="twitter:description" content={'When the sun fills up with helium, a solar flare shoots in a random direction, at one contributor'} />
-        <meta name="twitter:image" content="https://jdq7a-qiaaa-aaaad-qcbia-cai.ic.fleek.co/ogp.jpg" />
+        <meta name="twitter:image" content="https://sf.duelaliens.com/ogp.jpg" />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
 
